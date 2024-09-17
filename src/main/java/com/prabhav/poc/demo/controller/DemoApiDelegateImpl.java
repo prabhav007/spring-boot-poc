c53da30766c221f0ae436614070b2a62be7cf3c0
Prabhav Sunil <psunil@ripple.com>
2024-09-17 10:41:44 +0530

    Initial commit

---
+++ src/main/java/com/prabhav/poc/demo/controller/DemoApiDelegateImpl.java
@@ -0,0 +1,13 @@
+package com.prabhav.poc.demo.controller;
+
+import com.prabhav.poc.demo.DemoApiDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoApiDelegateImpl implements DemoApiDelegate {
+
+  @Override
+  public void readUser(String code) {
+    //
+  }
+}
